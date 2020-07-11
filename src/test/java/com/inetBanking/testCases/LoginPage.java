e5e7d3ccd92f167a5872da542eab25840d26219a
ohalsudarshan <ohalsudarshan@gmail.com>
2020-07-11 13:17:48 +0530

    new files

---
+++ src/test/java/com/inetBanking/testCases/LoginPage.java
@@ -0,0 +1,14 @@
+package com.inetBanking.testCases;
+
+import org.testng.annotations.Test;
+
+public class LoginPage extends BaseClass{
+	
+	@Test
+	public void login()
+	{
+		setup("firefox");
+		
+	}
+
+}
--- src/test/java/com/inetBanking/testCases/TC_LoginTest_001.java
+++ src/test/java/com/inetBanking/testCases/TC_LoginTest_001.java
@@ -11,9 +11,6 @@ public class TC_LoginTest_001 extends BaseClass{
 	@Test
 	public void loginTest() throws IOException 
 	{
-				
-		logger.info("URL is opened");
-		
 		LoginPage lp=new LoginPage(driver);
 		lp.setUsername(username);
 		logger.info("Entered Username ");
